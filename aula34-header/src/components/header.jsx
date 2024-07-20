fc599d29cd8cf1cc5f8eb3bee2f8170017b31bd6
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-07-20 11:55:01 -0300

    aula43-header

---
+++ aula34-header/src/components/header.jsx
@@ -0,0 +1,19 @@
+export function Header(){
+  return(
+    <header>
+        <div id="container-header">
+          <div id='logo-header'>Loading</div>
+          <nav id='nav-header'>
+            <ul>
+              <li>home</li>
+              <li>about</li>
+              <li>contact</li>
+            </ul>
+          </nav>
+          <div id="btn-header">
+            <button>log in</button>
+          </div>
+        </div>
+      </header>
+  )
+}
